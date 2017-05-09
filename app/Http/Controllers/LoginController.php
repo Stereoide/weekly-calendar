4093d665d6d44bcc2c6fc91eab1d2083eb231971
Jörn Maske <j.maske@otterbach.de>
2017-05-09 13:56:17 +0200

    Basic Google oAuth authentification

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Socialite;
+
+class LoginController extends Controller
+{
+    /**
+     * Redirect the user to the Google authentication page.
+     *
+     * @return Response
+     */
+    public function redirectToProvider()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return Response
+     */
+    public function handleProviderCallback()
+    {
+        $user = Socialite::driver('google')->user();
+
+        dd($user);
+
+        // $user->token;
+    }
+}
